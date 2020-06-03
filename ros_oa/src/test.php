06ee44365b573f676720dfd47f8e3326cb73bf9c
xiongyuhui <allanxyh@gmail.com>
2020-06-03 16:58:16 +0800

    test

---
+++ ros_oa/src/test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/6/3
+ * Time: 16:47
+ */
+namespace SanteOa;
+
+class Test
+{
+    public function test()
+    {
+        return 'hello';
+    }
+}
